a984c2c771e2fcb199d70ca411e33f8a6884a73e
Hatim <sakazokihatim@gmail.com>
2024-12-26 14:42:36 +0100

    ajout le code

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+const connectDB = require('./config/db');
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+connectDB();
+
+app.use('/api/task', taskRoutes);
+
+
+app.listen(port, () => {
+    console.log(`serveur ecouter le port ${port}`);
+});
