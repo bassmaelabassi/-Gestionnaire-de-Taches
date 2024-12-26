b77ce2217b6a7362a9a21fd7a54737bc2692e061
Hatim <sakazokihatim@gmail.com>
2024-12-26 14:48:12 +0100

    ajout code

---
+++ models/Task.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    completed: { type: Boolean, default: false }
+    });
+    const Task = mongoose.model('Task', taskSchema);
